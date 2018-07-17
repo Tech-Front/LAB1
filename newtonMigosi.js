47930983188b60b43ad70eca3409ac522eba5562
tonisheen <newtonmigosi@gmail.com>
2018-07-17 21:36:38 +0300

    implemented candy class

---
+++ newtonMigosi.js
@@ -0,0 +1,22 @@
+class Candy {
+    constructor(id, color) {
+        this.id = id;
+        this.color = color;
+        this.row = null;
+        this.column = null;
+    }
+
+    setPosition(row, column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    toString() {
+        return `${this.color} candy at r${this.row}, c${this.column}`;
+    }
+}
+
+candy = new Candy(1, 'red');
+candy.setPosition(0, 0);
+
+console.log(candy.toString());
